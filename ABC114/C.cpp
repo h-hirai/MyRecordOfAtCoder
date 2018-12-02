be415aa11586820adc4e142af23bc97d560e3ca7
h-hirai <hirai.hajime@gmail.com>
2018-12-02 21:28:24 +0900

    とりあえず列挙してみた

---
+++ ABC114/C.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+bool is753(size_t n) {
+  size_t cnt7 = 0;
+  size_t cnt5 = 0;
+  size_t cnt3 = 0;
+
+  while (n>0) {
+    switch (n % 10) {
+    case 7: cnt7++; break;
+    case 5: cnt5++; break;
+    case 3: cnt3++; break;
+    default: return false;
+    }
+    n/=10;
+  }
+  return cnt7 > 0 && cnt5 > 0 && cnt3 > 0;
+}
+
+int main() {
+  size_t N;
+
+  std::cin >> N;
+
+  for (size_t i=1; i<N+1; i++) {
+    if (is753(i)) std::cout << i << std::endl;
+  }
+
+  return 0;
+}
