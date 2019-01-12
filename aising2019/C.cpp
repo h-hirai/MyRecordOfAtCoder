1ffbe4bcac371539081bf5fa3520fd8482b2f74f
h-hirai <hirai.hajime@gmail.com>
2019-01-12 23:28:58 +0900

    ついでなのでrankあり版も書いてみたがなんかむしろ遅い

--- aising2019/C.cpp
+++ aising2019/C.cpp
@@ -3,15 +3,16 @@
 #include <map>
 
 struct UnionFindTree {
-  std::vector<long> par;
+  std::vector<size_t> par;
+  std::vector<size_t> rank;
 
-  UnionFindTree(long N) : par(N) {
-    for (long i=0; i<N; i++) {
+  UnionFindTree(size_t N) : par(N), rank(N) {
+    for (size_t i=0; i<N; i++) {
       par[i] = i;
     }
   }
 
-  long root(long x) {
+  size_t root(size_t x) {
     if (par[x] == x) {
       return x;
     } else {
@@ -19,15 +20,22 @@ struct UnionFindTree {
     }
   }
 
-  bool same(long x, long y) {
+  bool same(size_t x, size_t y) {
     return root(x) == root(y);
   }
 
-  void unite(long x, long y) {
-    long root_x = root(x);
-    long root_y = root(y);
+  void unite(size_t x, size_t y) {
+    size_t root_x = root(x);
+    size_t root_y = root(y);
     if (root_x == root_y) return;
-    par[root_x] = root_y;
+
+    if (rank[root_x] < rank[root_y])
+      par[root_x] = root_y;
+    else
+      par[root_y] = root_x;
+
+    if (rank[root_x] == rank[root_y])
+      rank[root_x]++;
   }
 };
 
