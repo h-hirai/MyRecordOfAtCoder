d76a48d45830dcee9e6a3c0eaa8e95c5b986184d
h-hirai <hirai.hajime@gmail.com>
2019-02-09 22:01:56 +0900

    AC

--- yahoo-procon2019-qual/C.cpp
+++ yahoo-procon2019-qual/C.cpp
@@ -7,17 +7,17 @@ int main() {
 
   std::cin >> K >> A >> B;
 
-  if (A + 2 < B) {
-    auto rest_K = K - A - 1;
-    auto ans = B + (rest_K / 2 * (B - A));
-    ans += rest_K % 2;
+  INT ans = 0;
 
-    std::cerr << rest_K << std::endl;
-    std::cout << ans << std::endl;
+  if (A + 2 < B && K >= A + 1) {
+    auto rest_K = K - A - 1;
+    ans = B + (rest_K / 2 * (B - A));
+    ans += rest_K % 2;
   } else {
-    auto ans = 1 + K;
-    std::cout << ans << std::endl;
+    ans = 1 + K;
   }
+
+  std::cout << ans << std::endl;
 
   return 0;
 }
