ad05c8696fc1cbe0ce4adaa22b65a38ec33b1193
h-hirai <hirai.hajime@gmail.com>
2019-03-15 21:20:26 +0900

    AC、priority_queueの優先度が逆だった

--- SoundHound2018/D.cpp
+++ SoundHound2018/D.cpp
@@ -10,7 +10,8 @@ constexpr size_t INF = std::numeric_limits<size_t>::max() / 2;
 
 std::vector<size_t> calc_fare(Graph_t const& g, size_t start) {
   std::vector<size_t> ret(g.size(), INF);
-  std::priority_queue<std::pair<size_t, size_t>> que;
+  using P = std::pair<size_t, size_t>;
+  std::priority_queue<P, std::vector<P>, std::greater<P>> que;
 
   ret[start] = 0;
   que.emplace(0, start);
