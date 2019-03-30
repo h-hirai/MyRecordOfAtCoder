c15fa03278239d78644b1586587bf74c790d92d7
h-hirai <hirai.hajime@gmail.com>
2019-03-31 03:58:41 +0900

    解説見て書いた二分探索版。RE。ありー?

--- exawizards2019/C.cpp
+++ exawizards2019/C.cpp
@@ -1,53 +1,79 @@
 #include <iostream>
-#include <vector>
+#include <string>
 #include <unordered_map>
-#include <vector>
+#include <map>
+
+using commands_t = std::unordered_map<char, std::map<size_t, bool>>;
+
+int simulate(int N, int i, int j,
+             std::string const& S, commands_t const& td) {
+  if (i < 0) return -1;
+  if (i >= N) return N;
+
+  auto const& cmds = td.at(S[i]);
+  auto it = cmds.lower_bound(j);
+
+  if (it == cmds.end()) return i;
+
+  j = it->first;
+
+  if (it->second)
+    return simulate(N, i-1, j+1, S, td);
+  else
+    return simulate(N, i+1, j+1, S, td);
+}
 
 int main() {
   size_t N, Q;
+  std::string S;
 
-  std::cin >> N >> Q;
+  std::cin >> N >> Q >> S;
 
-  std::unordered_map<char, std::vector<size_t>> Cells;
-
-  for (size_t i=0; i<N; i++) {
-    char c;
-    std::cin >> c;
-    Cells[c].push_back(i);
-  }
-
-  size_t ans = N;
-  std::vector<size_t> Golems(N, 1);
+  commands_t td;
 
   for (size_t i=0; i<Q; i++) {
     char t, d;
     std::cin >> t >> d;
 
-    auto const& cells = Cells[t];
+    td[t].emplace(i, d=='L');
+  }
 
-    if (d == 'L') {
-      for (auto j: cells) {
-        if (j == 0) {
-          ans -= Golems[j];
-        } else {
-          Golems[j-1] += Golems[j];
-        }
-        Golems[j] = 0;
-      }
-    } else {
-      for (auto it = cells.crbegin();
-           it != cells.crend();
-           it++) {
-        auto j = *it;
+  auto ans = N;
 
-        if (j == N-1) {
-          ans -= Golems[j];
-        } else {
-          Golems[j+1] += Golems[j];
-        }
-        Golems[j] = 0;
+  {
+    size_t first = 0;
+    size_t last = N;
+
+    for (auto len = last-first; len != 0; ) {
+      auto half = len / 2;
+      auto mid = first + half;
+      if (simulate(N, mid, 0, S, td) < 0) {
+        len -= half + 1;
+        first = mid + 1;
+      } else {
+        len = half;
       }
     }
+
+    ans -= first;
+  }
+
+  {
+    size_t first = 0;
+    size_t last = N;
+
+    for (auto len = last-first; len != 0; ) {
+      auto half = len / 2;
+      auto mid = first + half;
+      if (simulate(N, mid, 0, S, td) < static_cast<int>(N)) {
+        len -= half + 1;
+        first = mid + 1;
+      } else {
+        len = half;
+      }
+    }
+
+    ans -= N-first;
   }
 
   std::cout << ans << std::endl;
