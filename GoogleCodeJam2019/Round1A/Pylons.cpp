efa951b3001fdc98d93a883b1830917ccd65a188
h-hirai <hirai.hajime@gmail.com>
2019-04-13 11:52:35 +0900

    AC

--- GoogleCodeJam2019/Round1A/Pylons.cpp
+++ GoogleCodeJam2019/Round1A/Pylons.cpp
@@ -12,16 +12,58 @@ testcase(int R, int C) {
 
   for (int c=0; c<C; c++) {
     for (int r=0; r<R; r++) {
-      if (r % 2 == 0) {
-        ret.emplace_back(r, c);
+      int rr = (C == R && c == C-1 && C % 2 == 0) ? (r + 1) % R : r;
+      if (rr % 2 == 0) {
+        ret.emplace_back(rr, c);
       } else {
-        ret.emplace_back(r, (c + K) % C);
+        ret.emplace_back(rr, (c + K) % C);
       }
     }
   }
 
   return ret;
 }
+
+#include <iomanip>
+
+// int main() {
+//   for (int R=2; R<21; R++) {
+//     for (int C=R; C<21; C++) {
+//       auto ans = testcase(R, C);
+//       if (ans.empty()) continue;
+//       for (int i=1; i<R*C; i++) {
+//         auto ay = ans[i-1].first;
+//         auto ax = ans[i-1].second;
+//         auto by = ans[i].first;
+//         auto bx = ans[i].second;
+//         if (ay == by || ax == bx ||
+//             ay - ax == by - bx || ay + ax == by + bx) {
+//           std::cerr << "case " << R << ", " << C << " was failed." << std::endl;
+
+//           for (auto const& p: ans) {
+//             std::cerr << p.first << ',' << p.second << std::endl;
+//           }
+
+//           // std::vector<std::vector<int>> grid(R, std::vector<int>(C));
+//           // for (int i=0; i<R*C; i++)
+//           //   grid[ans[i].first][ans[i].second] = i;
+//           // for (int r=0; r<R; r++) {
+//           //   for (int c=0; c<C; c++) {
+//           //     std::cerr << std::setw(4) << grid[r][c];
+//           //   }
+//           //   std::cerr << std::endl;
+//           // }
+
+//           // break;
+//         }
+//       }
+//     }
+//   }
+
+//   return 0;
+// }
+
+#include <iomanip>
 
 int main() {
   size_t T;
