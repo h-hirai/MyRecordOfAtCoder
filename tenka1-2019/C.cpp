39d72238cfcbfb36188e9510c4cddae5916cf522
h-hirai <hirai.hajime@gmail.com>
2019-04-20 22:50:53 +0900

    WA

--- tenka1-2019/C.cpp
+++ tenka1-2019/C.cpp
@@ -7,6 +7,7 @@ int main() {
   int whites = 0;
   int blacks = 0;
 
+  int whites_of_left = 0;
   int blacks_of_right = 0;
 
   for (int i=0; i<N; i++) {
@@ -15,14 +16,16 @@ int main() {
 
     if (c == '.') {
       whites++;
-      blacks += blacks_of_right;
+      if (blacks == 0) whites_of_left++;
+
       blacks_of_right = 0;
     } else {
+      blacks++;
       blacks_of_right++;
     }
   }
 
-  int ans = std::min(whites, blacks);
+  int ans = std::min(whites-whites_of_left, blacks-blacks_of_right);
 
   std::cout << ans << '\n';
 
