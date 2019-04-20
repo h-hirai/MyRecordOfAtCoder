693851a51f2f0864a60350dda0ed719d35be7db9
h-hirai <hirai.hajime@gmail.com>
2019-04-20 21:20:10 +0900

    WA。まぬけ

---
+++ tenka1-2019/C.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+  int N;
+  std::cin >> N;
+
+  int whites = 0;
+  int blacks = 0;
+
+  int blacks_of_right = 0;
+
+  for (int i=0; i<N; i++) {
+    char c;
+    std::cin >> c;
+
+    if (c == '.') {
+      whites++;
+      blacks += blacks_of_right;
+    } else {
+      blacks_of_right++;
+    }
+  }
+
+  int ans = std::min(whites, blacks);
+
+  std::cout << ans << '\n';
+
+  return 0;
+}
