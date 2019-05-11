b619e683be89abf842fd3dfdb2c8bc21452ba873
h-hirai <hirai.hajime@gmail.com>
2019-05-11 22:37:58 +0900

    AC、時間かけすぎ

---
+++ diverta2019/B.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+
+int main() {
+  int R, G, B, N;
+  std::cin >> R >> G >> B >> N;
+
+  int ans = 0;
+
+  for (int r=0; r*R<=N; r++) {
+    for (int g=0; r*R+g*G<=N; g++) {
+      if ((N - (r*R+g*G)) % B == 0) {
+        ans++;
+      }
+    }
+  }
+
+  std::cout << ans << std::endl;
+
+  return 0;
+}
