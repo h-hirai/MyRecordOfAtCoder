0f984a10714a1d533cac7ae974694957dd56906c
h-hirai <hirai.hajime@gmail.com>
2019-06-02 23:10:43 +0900

    AC、10分遅れ

--- AGC034/B.cpp
+++ AGC034/B.cpp
@@ -3,17 +3,13 @@
 
 using ll = long long;
 
-ll count_a(std::string const& S, int i,
-           ll ret, ll cnt_a, bool flg_abc);
+ll count_a(std::string const& S, int i, ll ret, ll cnt_a);
+ll look_bc(std::string const& S, int i, ll ret, ll cnt_a);
 
-ll look_bc(std::string const& S, int i,
-           ll ret, ll cnt_a, bool flg_abc);
-
-ll neutral(std::string const& S, int i,
-           ll ret) {
+ll neutral(std::string const& S, int i, ll ret) {
   if (i < S.size()-1) {
     if (S[i] == 'A') {
-      return count_a(S, i+1, ret, 1, false);
+      return count_a(S, i+1, ret, 1);
     } else {
       return neutral(S, i+1, ret);
     }
@@ -22,14 +18,12 @@ ll neutral(std::string const& S, int i,
   }
 }
 
-ll count_a(std::string const& S, int i,
-           ll ret, ll cnt_a, bool flg_abc) {
+ll count_a(std::string const& S, int i, ll ret, ll cnt_a) {
   if (i < S.size()-1) {
     if (S[i] == 'A') {
-      return count_a(S, i+1, ret, cnt_a+1, flg_abc);
+      return count_a(S, i+1, ret, cnt_a+1);
     } else if (S[i] == 'B' && S[i+1] == 'C') {
-      if (flg_abc) ret+=1;
-      return look_bc(S, i+2, ret+cnt_a, cnt_a, true);
+      return look_bc(S, i+2, ret+cnt_a, cnt_a);
     } else {
       return neutral(S, i+1, ret);
     }
@@ -38,13 +32,12 @@ ll count_a(std::string const& S, int i,
   }
 }
 
-ll look_bc(std::string const& S, int i,
-           ll ret, ll cnt_a, bool flg_abc) {
+ll look_bc(std::string const& S, int i, ll ret, ll cnt_a) {
   if (i < S.size()-1) {
     if (S[i] == 'A') {
-      return count_a(S, i+1, ret, 1, true);
+      return count_a(S, i+1, ret, cnt_a+1);
     } else if (S[i] == 'B' && S[i+1] == 'C') {
-      return look_bc(S, i+2, ret+cnt_a, cnt_a, true);
+      return look_bc(S, i+2, ret+cnt_a, cnt_a);
     } else {
       return neutral(S, i+1, ret);
     }
